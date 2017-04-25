5933620ba26c77aad1683295507ce119854789ef
admin <b6511631@gmail.com>
2017-04-25 14:59:47 +0800

    初始化项目

---
+++ app/src/main/java/com/it520/yingke/MainActivity.java
@@ -0,0 +1,13 @@
+package com.it520.yingke;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
