502cae34c31bcb69f6ef1d34bec705cbacd6408f
admin <b6511631@gmail.com>
2017-04-25 20:40:43 +0800

    完成效果：主页面底部导航栏中，点击时能切换两个Fragment进行展示。

---
+++ app/src/main/java/com/it520/yingke/fragment/MineFragment.java
@@ -0,0 +1,20 @@
+package com.it520.yingke.fragment;
+
+import android.support.v4.app.Fragment;
+
+
+/* 
+ * ============================================================
+ * Editor: MuMuXuan(6511631@qq.com)
+ *  
+ * Time: 2017-04-25 16:11 
+ * 
+ * Description: 
+ *
+ * Version: 1.0
+ * ============================================================
+ */
+
+public class MineFragment extends Fragment {
+    
+}
